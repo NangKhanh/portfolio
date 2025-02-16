d282fe7ff2739d6329aa8ea77cb9864753c13b8f
Nang Khanh <nangkhan311202@gmail.com>
2025-02-16 10:34:53 +0700

    update inview animation

--- src/components/Sections/Skills.jsx
+++ src/components/Sections/Skills.jsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
-import { useMemo, useState } from "react";
+import { motion, useInView } from "framer-motion";
+import { useMemo, useState, useRef, useEffect } from "react";
 import { IconCloud } from "../magicui/icon-cloud";
 import TypewriterText from "../TypewriterText";
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaDatabase } from "react-icons/fa";
@@ -9,7 +9,10 @@ import { AnimatedSpan, Terminal, TypingAnimation } from "../magicui/terminal";
 export default function Skills() {
   const [flipped, setFlipped] = useState(false);
   const [animateSkills, setAnimateSkills] = useState(false);
-  const [terminalKey, setTerminalKey] = useState(0); // Key để force re-render Terminal
+  const [terminalKey, setTerminalKey] = useState(0);
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const terminalRef = useRef(null);
+  const isInView = useInView(terminalRef, { once: true });
 
   const slugs = useMemo(() => [
     "typescript", "javascript", "dart", "java", "react", "flutter", "android",
@@ -20,50 +23,32 @@ export default function Skills() {
   ], []);
 
   const images = useMemo(() => slugs.map(slug => `https://cdn.simpleicons.org/${slug}/${slug}`), [slugs]);
-  const myInformation = {
-    language: ["JavaScript", "TypeScript"],
-    front_end: {
-      technologies: ["ReactJS", "Redux Toolkit", "Ant Design"],
-      features: [
-        "Responsive with devices",
-        "Develop user-friendly interfaces"
-      ]
-    },
-    back_end: {
-      technologies: [
-        "ExpressJS", "NestJS", "MySQL", "MongoDB"
-      ],
-      features: [
-        "Design and develop APIs following REST standards",
-        "Optimize SQL queries"
-      ]
-    }
-  };
 
   const skills = [
-    // { name: "HTML", level: 80, icon: <FaHtml5 className="text-2xl text-orange-600" /> },
-    // { name: "CSS", level: 95, icon: <FaCss3Alt className="text-2xl text-blue-600" /> },
     { name: "JavaScript", level: 90, icon: <FaJs className="text-2xl text-yellow-500" /> },
     { name: "TypeScript", level: 70, icon: <SiTypescript className="text-2xl text-blue-500" /> },
     { name: "ReactJS", level: 95, icon: <FaReact className="text-2xl text-cyan-500" /> },
     { name: "NextJS", level: 75, icon: <SiNextdotjs className="text-2xl text-black dark:text-white" /> },
     { name: "ExpressJS", level: 65, icon: <SiExpress className="text-2xl text-gray-800 dark:text-white" /> },
     { name: "NestJS", level: 70, icon: <SiNestjs className="text-2xl text-red-600" /> },
-    // { name: "MySQL", level: 70, icon: <SiMysql className="text-2xl text-blue-700" /> },
-    // { name: "MongoDB", level: 65, icon: <SiMongodb className="text-2xl text-green-500" /> }
   ];
+
+  useEffect(() => {
+    if (isInView && !hasAnimated) {
+      setHasAnimated(true);
+      setTerminalKey(prev => prev + 1);
+    }
+  }, [isInView, hasAnimated]);
 
   const handleFlip = () => {
     setFlipped(!flipped);
     setAnimateSkills(!flipped);
-    setTerminalKey(prev => prev + 1);
-
   };
 
   return (
     <motion.section
       id="skills"
-      className="py-20 px-6 text-center bg-white dark:bg-gray-900 h-full "
+      className="py-20 px-6 text-center bg-white dark:bg-gray-900 h-full"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
@@ -71,6 +56,7 @@ export default function Skills() {
       <h2 className="text-4xl font-bold text-gray-900 dark:text-white">Skills</h2>
       <div className="flex-media">
         <div
+          ref={terminalRef}
           className="relative mt-6 p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow-md inline-block text-left font-mono whitespace-break-spaces cursor-pointer size-fit"
           style={{ height: "30rem", width: "480px", perspective: "1000px" }}
           onClick={handleFlip}
@@ -82,69 +68,74 @@ export default function Skills() {
             style={{ transformStyle: "preserve-3d" }}
           >
             <div className="absolute w-full h-full" style={{ backfaceVisibility: "hidden" }}>
-              {/* <TypewriterText key={flipped} text={[JSON.stringify(myInformation, null, 2)]} delay={20} /> */}
-              <Terminal key={terminalKey}>
-                <TypingAnimation>&gt; npm skills@latest init</TypingAnimation>
+              {isInView && (
+                <Terminal key={terminalKey}>
+                  <TypingAnimation>&gt; npm skills@latest init</TypingAnimation>
 
-                <AnimatedSpan delay={1500} className="text-green-500">
-                  <span>✔ Preflight checks.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={1500} className="text-green-500">
+                    <span>✔ Preflight checks.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={2000} className="text-yellow-500">
-                  <span>✔ Verifying language. Found JavaScript, TypeScript.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={2000} className="text-yellow-500">
+                    <span>✔ Verifying language. Found JavaScript, TypeScript.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={2500} className="text-yellow-500">
-                  <span>✔ Verifying FE framework. Found NextJs, ReactJs.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={2500} className="text-yellow-500">
+                    <span>✔ Verifying FE framework. Found NextJs, ReactJs.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={3000} className="text-yellow-500">
-                  <span>✔ Verifying BE framework. Found NestJs, ExpressJs.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={3000} className="text-yellow-500">
+                    <span>✔ Verifying BE framework. Found NestJs, ExpressJs.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={3500} className="text-green-500">
-                  <span>✔ Validating import alias.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={3500} className="text-green-500">
+                    <span>✔ Validating import alias.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={4000} className="text-green-500">
-                  <span>✔ Writing components.json.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={4000} className="text-green-500">
+                    <span>✔ Writing components.json.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={4500} className="text-green-500">
-                  <span>✔ Checking registry.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={4500} className="text-green-500">
+                    <span>✔ Checking registry.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={5000} className="text-green-500">
-                  <span>✔ Updating tailwind.config.ts</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={5000} className="text-green-500">
+                    <span>✔ Updating tailwind.config.ts</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={5500} className="text-green-500">
-                  <span>✔ Updating app/globals.css</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={5500} className="text-green-500">
+                    <span>✔ Updating app/globals.css</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={6000} className="text-green-500">
-                  <span>✔ Installing dependencies.</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={6000} className="text-green-500">
+                    <span>✔ Installing dependencies.</span>
+                  </AnimatedSpan>
 
-                <AnimatedSpan delay={6500} className="text-blue-500">
-                  <span>ℹ Updated 1 file:</span>
-                  <span className="pl-2">- lib/skills.ts</span>
-                </AnimatedSpan>
+                  <AnimatedSpan delay={6500} className="text-blue-500">
+                    <span>ℹ Updated 1 file:</span>
+                    <span className="pl-2">- lib/skills.ts</span>
+                  </AnimatedSpan>
 
-                <TypingAnimation delay={7000} className="text-muted-foreground">
-                  Success! Project initialization completed.
-                </TypingAnimation>
+                  <TypingAnimation delay={7000} className="text-muted-foreground">
+                    Success! Project initialization completed.
+                  </TypingAnimation>
 
-                <TypingAnimation delay={7500} className="text-muted-foreground">
-                  You may now contact me.
-                </TypingAnimation>
+                  <TypingAnimation delay={7500} className="text-muted-foreground">
+                    You may now contact me.
+                  </TypingAnimation>
 
-                <TypingAnimation delay={7500} className="text-  -foreground">
-                  Click here to explore.
-                </TypingAnimation>
-              </Terminal>
+                  <TypingAnimation delay={8000} className="text-muted-foreground">
+                    Click here to explore.
+                  </TypingAnimation>
+                </Terminal>
+              )}
             </div>
-            <div className="absolute w-full h-full flex flex-col items-start justify-center p-6 bg-gray-100 dark:bg-gray-800 rounded-lg" style={{ transform: "rotateY(180deg)", backfaceVisibility: "hidden" }}>
+
+            <div
+              className="absolute w-full h-full flex flex-col items-start justify-center p-6 bg-gray-100 dark:bg-gray-800 rounded-lg"
+              style={{ transform: "rotateY(180deg)", backfaceVisibility: "hidden" }}
+            >
               {skills.map((skill, index) => (
                 <div key={index} className="w-full mb-4">
                   <div className="flex justify-between text-sm font-medium text-gray-900 dark:text-gray-200">
@@ -165,7 +156,7 @@ export default function Skills() {
           </motion.div>
         </div>
 
-        <div className="relative flex items-center justify-center overflow-hidden mt-10  side-image">
+        <div className="relative flex items-center justify-center overflow-hidden mt-10 side-image">
           <IconCloud images={images} />
         </div>
       </div>
